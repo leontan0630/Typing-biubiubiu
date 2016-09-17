a58c49a4d05186eaa1c7bad2b6a5aa810fb5af74
rundali <rundali@yahoo.com>
2016-09-17 17:11:39 -0400

    newframe

---
+++ Project442/src/newFrame.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class newFrame {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					newFrame window = new newFrame();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public newFrame() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
